2173a30b8f22f8c4b806223ae9ad7c639c25d376
demah2 <deema.nxt@gmail.com>
2023-02-04 15:25:16 +0300

    n

--- src/main/java/process.java
+++ src/main/java/process.java
@@ -1,74 +1,71 @@
-
-import java.io.Serializable;
-
-public class process implements Serializable{
-    public static int counter=1;
-    private int pid;
-    private int priority;//1-5
-    private int arrival_time;
-    private int CPU_burst_time;
-    private int cpu_b;//edit may be removed 
-    private int start_time;
-    private int terminat_time;
-    private int turn_around_time;
-    private int Waiting_time;
-    private int Response_time;
+public class process {
+    public static int counter=1;//to set pid value
+    public int pid;
+    public int priority;//1-5
+    public int arrival_time;
+    public int CPU_burst_time;
+    public int remaining_time;
+    public int terminat_time;
+    public int turn_around_time;
+    public int Waiting_time;
+    public int Response_time;
+    public int start_time;
+    public int readyQEntryTime;
     
-    public process(int priority, int arrival_time, int CPU_burst_time) {
-        this.pid=counter;
+     public process(int priority, int arrival_time, int CPU_burst_time) {
+        this.pid=counter++;
         this.priority = priority;
         this.arrival_time = arrival_time;
         this.CPU_burst_time = CPU_burst_time;
-        this.cpu_b= CPU_burst_time;
-        counter++;
+        this.remaining_time =CPU_burst_time;
+        this.Response_time=-1;
+        this.Waiting_time=-1;
+        this.start_time=-1;
+        this.terminat_time=-1;
+        this.turn_around_time=-1;
+        this.readyQEntryTime=0;
+     }
+
+    public void setRemaining_time(int remaining_time) {
+        this.remaining_time = remaining_time;
+    }
+
+    public void setStart_time(int start_time) {
+        this.start_time = start_time;
+    }
+
+    public void setTerminat_time(int terminat_time) {
+        this.terminat_time = terminat_time;
+    }
+
+    public void setResponse_time(int Response_time) {
+        this.Response_time = Response_time;
+    }
+
+    public void setTurn_around_time(int turn_around_time) {
+        this.turn_around_time = turn_around_time;
+    }
+
+    public void setWaiting_time(int Waiting_time) {
+        this.Waiting_time = Waiting_time;
+    }
+
+    public void setReadyQEntryTime(int readyQEntryTime) {
+        this.readyQEntryTime = readyQEntryTime;
     }
 
     @Override
     public String toString() {
-        return "process{" + "pid=" + pid + ", priority=" + priority + ", arrival_time=" + arrival_time + ", CPU_burst_time=" + CPU_burst_time + '}';
-    }//(task )add the rest if the varibles and make the format similer to the repot in the pdf 
-    //make it as orderd as you can :) not like mine
-
-    public int getPriority() {
-        return priority;
+        return    "Pid: " + pid+ "\n" 
+                        + "     CPU Burst Time: " +  CPU_burst_time  + "\n"
+                        + "     Priority: " +    priority      + "\n"
+                        + "     Arrival Time: " + arrival_time  + "\n"
+                        + "     Start Time: " + start_time + "\n"
+                        + "     Terminate Time: " + terminat_time + "\n"
+                        + "     TurnAround Time: " + turn_around_time  + "\n"
+                        + "     Waiting Time: " + Waiting_time + "\n"
+                        + "     Response Time: " + Response_time +"\n"; 
     }
-
-    public int getCpu_b() {
-        return cpu_b;
-    }
-
-    public void setCpu_b(int cpu_b) {
-        this.cpu_b = cpu_b;
-    }
-
-    public int getPid() {
-        return pid;
-    }
-
-    public int getArrival_time() {
-        return arrival_time;
-    }
-//(task) calculat  these varibles
-    // from PCB array(
-    public int getStart_time() {
-        return start_time;
-    }
-
-    public int getTerminat_time() {
-        return terminat_time;
-    }
-
-    public int getTurn_around_time() {
-        return turn_around_time;
-    }
-
-    public int getWaiting_time() {
-        return Waiting_time;
-    }
-
-    public int getResponse_time() {
-        return Response_time;
-    }
-//)    
-  
+     
+     
 }
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,19 +1,23 @@
-
-
 import java.io.*;
-import java.util.*;
+
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+        
 public class test {
-    public static process readyQ[]=new process[30];
+
+    
+    public static int fixedTimer;
+    public static int timer;
+    public static process readyQ[]= new process[30];
     public static process PCB[]=new process[100];
     public static int PCBcounter=0;
     public static int readyCounter=0;
     public static int P=0;
-
-    
-    public static void main(String []args){
+    public static double AVGwating=0;
+    public static double AVGturnAround=0;
+    public static double AVGresponce=0;
+    public static void main(String[]args){
     Scanner read = new Scanner(System.in);
     
     int ch=-1;
@@ -22,24 +26,24 @@ public class test {
     do{
         
         System.out.println("choose one of the following options by entering its number");
-        System.out.println("1.to create new processes ");
-        System.out.println("2.to display detailed information about each process in the system");
-        System.out.println("3.to exit");
-        
+        System.out.println("------------------------------");
+        System.out.println("1)to create new processes ");
+        System.out.println("2)to display detailed information about each process in the system");
+        System.out.println("3)to exit");
+        System.out.println("------------------------------");
         ch=read.nextInt();
     if(ch<1||ch>3)
       System.out.println("invaild option");
     
-    // (task)try /catch for wrong input such as letter  ...
+  
     if (ch==1){
         System.out.println("enter the number of process you want to create");
         P=read.nextInt();
-        
         for(int i=0;i<P;i++){
-        System.out.println("enter process priority priority range from 1 to 5 ");
+        System.out.println("enter process priority (range from 1 to 5) ");
             int prior=read.nextInt();
             while (prior<1 ||prior>5 ){
-                System.out.println("please enter priority in range from 1 to 5");
+                System.out.println("please enter priority  (range from 1 to 5) ");
                 prior=read.nextInt();
              } 
         System.out.println("enter arrival time");
@@ -49,117 +53,173 @@ public class test {
         p = new process(prior,arrival,cpu);
         readyQ[readyCounter++]=p;
         }
-       writeInFile();
-       margeSort(0,P-1);
-       appedToPCB();
-    }
+        margeSort(0,P-1);
+        fixedTimer=timer=readyQ[0].arrival_time;          
+        appedToPCB();
+        calculateAVG();
+        writeInFile();
+  
+    }             
     if(ch==2){
-     readFromFile();
-     printPCB();
+       readFromFile();
     }
-    
     }while(ch!=3);
     }
-    
+    ////////////////////////////////////    
     public static void margeSort(int l,int r){
-   if(l>=r)
-       return;
-   int m=(l+r)/2;
-   margeSort(l,m);
-   margeSort(m+1,r);
-   marge(l,m,r);
-      
+        if(l>=r)
+            return;
+        int m=(l+r)/2;
+        margeSort(l,m);
+        margeSort(m+1,r);
+        marge(l,m,r);
     }
     public static void marge(int l,int m,int r){
-    process B []= new process[r-l+1];
-   int i=l,j=m+1,k=0;
-     while(i<=m && j<=r){
-     if(readyQ[i].getPriority()<readyQ[j].getPriority()){
-     B[k++]=readyQ[i];
-     i++;
-     }
-     else if(readyQ[i].getPriority()==readyQ[j].getPriority()){
-       if(readyQ[i].getArrival_time()<readyQ[j].getArrival_time()){
-            B[k++]=readyQ[i];
-            i++;
-          }
-        else{
-            B[k++]=readyQ[j];
-            j++;        
-     }
-     }
-     else{
-     B[k++]=readyQ[j];
-     j++;        
-     }
-     }
-     if(i>m)
-        while(j<=r){
-     B[k++]=readyQ[j];
-     j++;
-     }
-     else   
-        while(i<=m){
-     B[k++]=readyQ[i];
-     i++;     
-    }
-     for(k=0;k<B.length;k++)
-     readyQ[k+l]=B[k];
-    }
+         process B []= new process[r-l+1];
+         int i=l,j=m+1,k=0;
+         while(i<=m && j<=r){
+         if(readyQ[i].arrival_time<readyQ[j].arrival_time){
+         B[k++]=readyQ[i];
+         i++;
+         } 
+         else if(readyQ[i].arrival_time==readyQ[j].arrival_time){
+             if(readyQ[i].pid<readyQ[j].pid){
+                  B[k++]=readyQ[i];
+                  i++;
+                }
+              else{
+                  B[k++]=readyQ[j];
+                  j++;        
+         }
+         }
+         else{
+         B[k++]=readyQ[j];
+         j++;        
+         }
+         }
+         if(i>m)
+            while(j<=r){
+         B[k++]=readyQ[j];
+         j++;
+         }
+         else   
+            while(i<=m){
+         B[k++]=readyQ[i];
+         i++;     
+        }
+         for(k=0;k<B.length;k++)
+         readyQ[k+l]=B[k];
+    } 
     
-    public static void appedToPCB(){ 
-        int totalBurests=totalCpuB();
-        int n=0;//counter for readyQ 
-        int i=0;
-         while(true){
-         if(n<P && readyQ[n].getCpu_b()>0){
-            int Cpu_b=readyQ[n].getCpu_b(); //get the remaing cpu burest for the process
-            readyQ[n].setCpu_b(Cpu_b-1); //update the remaing time by subtracting one (cpu_burest -1)
-            PCB[PCBcounter++]=readyQ[n++];
-            i++;   
-         }
-          if (n==P){
-         n=0;//the counter will be reset to zero if its value equlas to the counter of readyQ
-         }
-         
-         if(readyQ[n].getCpu_b()==0){
-         n++;
-         }
-         
-         if(i==totalBurests){
-         break;
-         }
-            
-     }
-         readyCounter=0;
-    }
-    
-    private static int totalCpuB(){//to calcualte the sum of cpu burests
-        int sum=0;
-    for(int i=0;i<P;i++){
-        sum+=readyQ[i].getCpu_b();
-    }
-     return sum;
-    }
-    
-    public static void printPCB(){
-        int i=0;
-    while(PCB[i]!=null){
-    System.out.print("P"+PCB[i].getPid()+"|CS|");
+    /////////////////////////////////
+    public static void appedToPCB(){     
+        int pointer=0;
+        int i;
+        boolean flag=true; 
+    while(flag){
+        if(readyQ[pointer].remaining_time==0){//this to not get stock in the same process in case it has the hightest priorioty
+            i=0;   
+             while(i<P && readyQ[i].arrival_time<=timer){ 
+                 if(readyQ[i].remaining_time>0){//geve the pointer to any other process we will deal with the highest proiority below
+                      pointer=i;
+                 break;
+             }
+                 i++;
+             }
+         }   
+       i=0;
+     while(i<P && readyQ[i].arrival_time<=timer){ //change the pointer to the highest priority process
+     if(readyQ[i].remaining_time>0 && (readyQ[i].priority<readyQ[pointer].priority || (readyQ[i].priority==readyQ[pointer].priority && readyQ[i].readyQEntryTime > readyQ[pointer].readyQEntryTime)))
+         pointer=i;   
     i++;
     }
-    System.out.print("\n");
+     
+    if(PCBcounter!=0 && PCB[PCBcounter-1].pid==readyQ[pointer].pid){//if same as priveous process
+    readyQ[pointer].setRemaining_time(readyQ[pointer].remaining_time-1);//just update remaning time
+    }            
+    else{//if it is not the same privous process
+      if(PCBcounter!=0)// no cs in the first time    
+        timer++;//cs
+      
+      while(i<P && readyQ[i].arrival_time<=timer){
+        if(readyQ[i].arrival_time==timer){
+        readyQ[i].setReadyQEntryTime(timer);
+        
+        }
+      i++;
+      }
+    readyQ[pointer].setRemaining_time(readyQ[pointer].remaining_time-1);//update remaning time
+    PCB[PCBcounter++]=readyQ[pointer];//add to process control block
+      if(PCB[PCBcounter-1].start_time==-1){//if first time a process enters set start time as the value of timer
+        PCB[PCBcounter-1].setStart_time(timer);
+        }
+      if(PCB[PCBcounter-1].Response_time==-1){
+      calculateResponceTime();
+      }
     }
-    // these are for file manipulation (all done)
-    public static void writeInFile(){
+     timer++;//normal timer
+     
+    if(PCB[PCBcounter-1].terminat_time==-1&& PCB[PCBcounter-1].remaining_time==0){//if on more remaning time set terminat time value
+        PCB[PCBcounter-1].setTerminat_time(timer);
+    }
+    if(PCB[PCBcounter-1].Waiting_time==-1&& PCB[PCBcounter-1].remaining_time==0){//if on more remaning time set wating time value
+    calculateWatingTimeAndTurnAround();
+        }
+    
+    flag=false;
+    for(i=0;i<P;i++){
+    if(readyQ[i].remaining_time>0){//if any process still have some remaing time do not exit
+        flag=true;
+        break;
+    }
+    }
+    
+    }
+}
+    ///////////////////////////
+    
+    public static void printall(){
+    for(int i=0;i<PCBcounter;i++){
+    System.out.print("P"+PCB[i].pid+"|CS|");
+    }
+    
+    }
+    
+          public static void writeInFile(){
         try {
             File f = new File("report1.txt");
-            FileOutputStream f2 = new FileOutputStream(f,true);
+            FileOutputStream f2 = new FileOutputStream(f);
             PrintWriter pr= new PrintWriter(f2);
-            
-            for(int i=0;i<P;i++){
-               pr.println(readyQ[i].toString());
-                        }
+                           int i=1;
+                    int n;
+                    while(i!=process.counter){
+                        n=0;
+                        while(PCB[n]!=null){
+                    if(PCB[n].pid==i){
+                    pr.print(PCB[n]);
+                    i++;
+                    break;
+                    }
+                    n++;
+                    }
+                    }
+                    //
+                    i=0;
+                    String str="[";
+                    
+                    while(PCB[i]!=null){
+                    str+="P"+PCB[i].pid+"|CS|";
+                    i++;
+                    }
+                    str=str.substring(0, str.length() - 4);
+                    str+="]";
+                    pr.println( "---------------------------------");
+                    pr.println(str);
+                    pr.println( "---------------------------------");
+                    pr.println(" Average  Turn around time= "+AVGturnAround);
+                    pr.println(" Average  Waiting time= "+AVGwating);
+                    pr.println(" Average  Response time= "+AVGresponce);
+                    pr.println( "---------------------------------");
               pr.close();
         } catch (FileNotFoundException ex) {
             Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
@@ -167,23 +227,111 @@ public class test {
             Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-     // these are for file manipulation (all done)   
-   public static void readFromFile(){
+ 
+    //
+      public static void readFromFile(){
       try{
-        
         File f = new File("report1.txt");
         Scanner sc = new Scanner(f);
-           
-                while(sc.hasNext()){
+             while(sc.hasNext()){
                 System.out.println(sc.nextLine());
-                }
-             sc.close();
               }
-   
-      catch(IOException ex){
-
-      }
+             sc.close();
+        }catch(IOException ex){
+         Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+     }
+    }
+    //
     
-   }
+    public static void calculateResponceTime(){ 
+        boolean flag=false;    
+        int arr[]=new int[PCBcounter] ;  
+        int n=0;
+        int j=0;
+        int i=0;
+        int responceCounter=0;
+        while(PCB[i].pid!=PCB[PCBcounter-1].pid){  
+            j=0;
+            while(j<n){
+            if(arr[j]==PCB[i].pid)
+                flag=true;
+            j++;
+            }
+        if(!flag){    
+        responceCounter+=Math.abs(PCB[i].remaining_time-PCB[i].CPU_burst_time);//=runing time for other processes
+        responceCounter++;//+CS time
+        arr[n++]=PCB[i].pid;
+        i++;
+        }
+        if(flag){
+        responceCounter++;//+CS time
+        i++;
+        }
+        flag=false;
+        }
+        PCB[PCBcounter-1].setResponse_time((fixedTimer+responceCounter)-PCB[PCBcounter-1].arrival_time);  
+    } 
+      
+      
+        public static void calculateWatingTimeAndTurnAround(){
+               boolean flag=false;    
+        int arr[]=new int[PCBcounter] ;  
+        int n=1;
+        int j=0;
+        int i=0;
+        int watingCounter=0;
+        arr[0]=PCB[PCBcounter-1].pid;
+        while(i<PCBcounter-1){  
+            j=0;
+            while(j<n){
+            if(arr[j]==PCB[i].pid)
+                flag=true;
+            j++;
+            }
+        if(!flag){    
+        watingCounter+=Math.abs(PCB[i].remaining_time-PCB[i].CPU_burst_time);//=runing time for other processes
+        watingCounter++;//+CS time
+        arr[n++]=PCB[i].pid;
+        i++;
+        }
+        if(flag){
+        watingCounter++;//+CS time
+        i++;
+        }
+        flag=false;
+        }
+        PCB[PCBcounter-1].setWaiting_time((fixedTimer+watingCounter)-PCB[PCBcounter-1].arrival_time);
+        PCB[PCBcounter-1].setTurn_around_time(PCB[PCBcounter-1].Waiting_time+PCB[PCBcounter-1].CPU_burst_time);
+        }
+ 
+    ////
+           public static void calculateAVG(){
+    int m=process.counter-1;
+    int n;
+    double sum1=0;
+    double sum2=0;
+    double sum3=0;
+    int i=1;
+    while(i!=process.counter){
+        n=0;
+      while(PCB[n]!=null){
+        if(PCB[n].pid==i){
+        sum1+=PCB[n].Response_time;
+        sum2+=PCB[n].turn_around_time;
+        sum3+=PCB[n].Waiting_time;
+        i++;
+        break;
+        }
+     n++;
+    }
+    } 
+
+    AVGwating=sum3/m;
+    AVGturnAround=sum2/m;
+    AVGresponce=sum1/m;
+    }
+      
 }
+   
+
 
